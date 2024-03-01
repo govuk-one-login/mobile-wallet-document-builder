9cd9fd65ab91490dcebf952aa206c6c6b091e2f4
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-01 11:42:38 +0000

    test(DCMAW-8406): unit test aws.ts

---
+++ test/config/appConfig.test.ts
@@ -0,0 +1,49 @@
+import {
+  getAwsRegion,
+  getDocumentsTableName,
+  getEnvironment,
+  getLocalStackEndpoint,
+  getPortNumber
+} from "../../src/config/appConfig";
+
+describe("aws.ts", () => {
+  it("should throw an error if PORT environment variable is not set", () => {
+    expect(() => getPortNumber()).toThrow(
+      new Error("PORT environment variable not set")
+    );
+  });
+
+  it("should return PORT environment variable value if set", () => {
+    process.env.PORT = "8000";
+    expect(getPortNumber()).toEqual("8000");
+  });
+
+  it("should throw an error if DOCUMENTS_TABLE_NAME environment variable is not set", () => {
+    expect(() => getDocumentsTableName()).toThrow(
+        new Error("DOCUMENTS_TABLE_NAME environment variable not set")
+    );
+  });
+
+  it("should return DOCUMENTS_TABLE_NAME environment variable value if set", () => {
+    process.env.DOCUMENTS_TABLE_NAME = "testTable";
+    expect(getDocumentsTableName()).toEqual("testTable");
+  });
+
+  it("should return the default value ('local') of the ENVIRONMENT environment variable value if not set", () => {
+    process.env.ENVIRONMENT = "";
+    expect(getEnvironment()).toEqual("local");
+  });
+
+  it("should return ENVIRONMENT environment variable value if set", () => {
+    process.env.ENVIRONMENT = "local";
+    expect(getEnvironment()).toEqual("local");
+  });
+
+  it("should return the AWS region value", () => {
+    expect(getAwsRegion()).toEqual("eu-west-2");
+  });
+
+  it("should return the LocalStack endpoint value", () => {
+    expect(getLocalStackEndpoint()).toEqual("http://localhost:4566");
+  });
+});
