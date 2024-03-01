7275d40f05e5ccf23a3a4d78d7cfbde481c76bb3
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-01 15:42:00 +0000

    feat(DCMAW-8406): trim request body

--- src/documentBuilder/router.ts
+++ src/documentBuilder/router.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { documentBuilderGet, documentBuilderPost } from "./controller";
+import { body } from "express-validator";
 
 const router = express.Router();
 
 router.get("/credential_offer", documentBuilderGet);
-router.post("/credential_offer", documentBuilderPost);
+router.post(
+  "/credential_offer",
+  [
+    body("title").trim(),
+    body("givenName").trim(),
+    body("familyName").trim(),
+    body("nino").trim(),
+  ],
+  documentBuilderPost
+);
 
 export { router as documentBuilderRouter };
