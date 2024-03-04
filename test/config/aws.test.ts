c85460ab452c8e7c80668ff894e6fbe0a864fd91
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-04 14:25:30 +0000

    test(DCMAW-8406): fix aws.ts unit tests

--- test/config/aws.test.ts
+++ test/config/aws.test.ts
@@ -6,25 +6,29 @@ import {
 describe("aws.ts", () => {
   it("should return the LocalStack AWS configuration", () => {
     expect(getLocalStackAwsConfig()).toStrictEqual({
-      accessKeyId: "accessKeyId",
+      credentials: {
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey",
+      },
       endpoint: "http://localhost:4566",
       region: "eu-west-2",
-      secretAccessKey: "secretAccessKey",
     });
   });
 
   it("should return the database configuration for the 'local' environment", () => {
     process.env.ENVIRONMENT = "local";
-    expect(getDatabaseConfig()).toEqual({
-      accessKeyId: "accessKeyId",
+    expect(getDatabaseConfig()).toStrictEqual({
+      credentials: {
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey",
+      },
       endpoint: "http://localhost:4566",
       region: "eu-west-2",
-      secretAccessKey: "secretAccessKey",
     });
   });
 
   it("should return the database configuration for the 'development' environment", () => {
     process.env.ENVIRONMENT = "development";
-    expect(getDatabaseConfig()).toEqual({ region: "eu-west-2" });
+    expect(getDatabaseConfig()).toStrictEqual({ region: "eu-west-2" });
   });
 });
