b59c22aa52277c3ca10e1a45136794da387f67b3
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-04 14:30:33 +0000

    test(DCMAW-8406): ignore server.ts in test coverage

--- jest.config.ts
+++ jest.config.ts
@@ -19,6 +19,7 @@ export default {
   ],
   "coveragePathIgnorePatterns": [
     "<rootDir>/src/types/interfaces.ts",
+    "<rootDir>/src/server.ts"
   ],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
