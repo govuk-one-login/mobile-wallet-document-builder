bacf39459be81fd8bfc236742d1ba7ff2385b4a7
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-13 16:35:08 +0000

    feat(DCMAW-8409): add an error page

--- src/documentBuilder/controller.ts
+++ src/documentBuilder/controller.ts
@@ -9,28 +9,37 @@ export async function documentBuilderGet(
   req: Request,
   res: Response
 ): Promise<void> {
-  console.log("documentBuilderGet");
-  res.render("document-form.njk");
+  try {
+    console.log("documentBuilderGet");
+    res.render("document-form.njk");
+  } catch (error) {
+    console.log(`An error happened: ${JSON.stringify(error)}`);
+    res.render("500.njk");
+  }
 }
 
 export async function documentBuilderPost(
   req: Request,
   res: Response
 ): Promise<void> {
-  console.log("documentBuilderPost");
+  try {
+    console.log("documentBuilderPost");
+    const document = Document.fromRequestBody(req.body);
+    const walletSubjectId = "walletSubjectIdPlaceholder";
+    const documentId = randomUUID();
+    await saveDocument(document, documentId, walletSubjectId);
 
-  const document = Document.fromRequestBody(req.body);
-  const walletSubjectId = "walletSubjectIdPlaceholder";
-  const documentId = randomUUID();
-  await saveDocument(document, documentId, walletSubjectId);
+    const response = await getCredentialOffer(walletSubjectId, documentId);
+    const credentialOfferUri = response["credential_offer_uri"];
+    const qrCode = await QRCode.toDataURL(credentialOfferUri);
 
-  const response = await getCredentialOffer(walletSubjectId, documentId);
-  const credentialOfferUri = response["credential_offer_uri"];
-  const qrCode = await QRCode.toDataURL(credentialOfferUri);
-
-  res.render("credential-offer.njk", {
-    universalLink: credentialOfferUri,
-    qrCode,
-    documentId, // Included for testing purposes only
-  });
+    res.render("credential-offer.njk", {
+      universalLink: credentialOfferUri,
+      qrCode,
+      documentId, // Included for testing purposes only
+    });
+  } catch (error) {
+    console.log(`An error happened: ${JSON.stringify(error)}`);
+    res.render("500.njk");
+  }
 }
