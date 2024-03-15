a0f52d67005c4cd932a7514edb01f47916c642b8
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-15 16:31:03 +0000

    test(DCMAW-8410): test getKmsConfig function

--- test/config/aws.test.ts
+++ test/config/aws.test.ts
@@ -1,6 +1,6 @@
 import {
   getLocalStackAwsConfig,
-  getDatabaseConfig,
+  getDatabaseConfig, getKmsConfig,
 } from "../../src/config/aws";
 
 describe("aws.ts", () => {
@@ -30,5 +30,22 @@ describe("aws.ts", () => {
   it("should return the database configuration for the 'development' environment", () => {
     process.env.ENVIRONMENT = "development";
     expect(getDatabaseConfig()).toStrictEqual({ region: "eu-west-2" });
+  });
+
+  it("should return the KMS configuration for the 'local' environment", () => {
+    process.env.ENVIRONMENT = "local";
+    expect(getKmsConfig()).toStrictEqual({
+      credentials: {
+        accessKeyId: "accessKeyId",
+        secretAccessKey: "secretAccessKey",
+      },
+      endpoint: "http://localhost:4561",
+      region: "eu-west-2",
+    });
+  });
+
+  it("should return the KMS configuration for the 'development' environment", () => {
+    process.env.ENVIRONMENT = "development";
+    expect(getKmsConfig()).toStrictEqual({ region: "eu-west-2" });
   });
 });
