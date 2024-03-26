be7621449215de3bb2dea8474f61886fa22cd977
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-26 14:35:35 +0000

    test(DCMAW-8483): test kmsService new method

--- test/services/kmsService.test.ts
+++ test/services/kmsService.test.ts
@@ -1,5 +1,5 @@
 import { mockClient } from "aws-sdk-client-mock";
-import { KMSClient, SignCommand } from "@aws-sdk/client-kms";
+import {GetPublicKeyCommand, KMSClient, SignCommand} from "@aws-sdk/client-kms";
 import { KmsService } from "../../src/services/kmsService";
 
 const mockKmsClient = mockClient(KMSClient);
@@ -16,8 +16,9 @@ describe("kmsService.ts", () => {
     mockKmsClient.reset();
   });
 
-  it("should throw an error when an error happens when calling KMS", async () => {
+  it("should throw an error when an error happens when calling KMS to sign token", async () => {
     mockKmsClient.on(SignCommand).rejects(new Error("SOME_KMS_ERROR"));
+
     await expect(kmsService.sign("mock_message_to_sign")).rejects.toThrow(
       "SOME_KMS_ERROR"
     );
@@ -25,13 +26,7 @@ describe("kmsService.ts", () => {
 
   it("should throw an error when KMS response has no signature", async () => {
     mockKmsClient.on(SignCommand).resolves({ Signature: undefined });
-    await expect(kmsService.sign("mock_message_to_sign")).rejects.toThrow(
-      "No signature returned"
-    );
-  });
 
-  it("should throw an error when KMS response has no signature", async () => {
-    mockKmsClient.on(SignCommand).resolves({ Signature: undefined });
     await expect(kmsService.sign("mock_message_to_sign")).rejects.toThrow(
       "No signature returned"
     );
@@ -43,5 +38,43 @@ describe("kmsService.ts", () => {
     const response = await kmsService.sign("mock_message_to_sign");
 
     expect(response).toEqual("bW9ja19zaWduYXR1cmU");
+  });
+
+  it("should throw an error when an error happens when calling KMS to fetch public key" , async () => {
+    mockKmsClient.on(GetPublicKeyCommand).rejects(new Error("SOME_KMS_ERROR"));
+
+    await expect(kmsService.getPublicKey()).rejects.toThrow(
+        "SOME_KMS_ERROR"
+    );
+  });
+
+  it("should throw an error when KMS response has no public key", async () => {
+    mockKmsClient.on(GetPublicKeyCommand).resolves({
+      KeyId: "arn:aws:kms:eu-west-2:000000000000:key/2ced22e2-c15b-4e02-aa5f-7a10a2eaccc7",
+      PublicKey: undefined,
+      KeySpec: 'RSA_4096',
+      KeyUsage: "SIGN_VERIFY",
+      SigningAlgorithms: ["RSASSA_PKCS1_V1_5_SHA_256"],
+    });
+
+    await expect(kmsService.getPublicKey()).rejects.toThrow(
+        "No public key returned"
+    );
+  });
+
+  it("should return a base64 encoded public key when call to KMS is successful", async () => {
+    mockKmsClient.on(GetPublicKeyCommand).resolves({
+      KeyId: "arn:aws:kms:eu-west-2:000000000000:key/2ced22e2-c15b-4e02-aa5f-7a10a2eaccc7",
+      PublicKey: Buffer.from(
+          "MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAuA1gxsWNOVSboz38+wAAeqKjq+yudtNpfg+xUuLKDLp+KcvYU84oSlxe1h4cCAwEAAQ==",
+          "base64"
+      ),
+      KeySpec: 'RSA_4096',
+      KeyUsage: "SIGN_VERIFY",
+      SigningAlgorithms: ["RSASSA_PKCS1_V1_5_SHA_256"],
+    });
+    const response = await kmsService.getPublicKey();
+
+    expect(response).toEqual("MIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAuA1gxsWNOVSboz38+wAAeqKjq+yudtNpfg+xUuLKDLp+KcvYU84oSlxe1h4cCAwEAAQ=");
   });
 });
