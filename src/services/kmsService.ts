9a2bb27ac1ae26fa917b5653690ee1336cdeb469
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-03-26 14:03:17 +0000

    refactor(DCMAW-8483): move kmsService to src/services and add new method to get public key

---
+++ src/services/kmsService.ts
@@ -0,0 +1,68 @@
+import {
+  GetPublicKeyCommand,
+  GetPublicKeyResponse,
+  KMSClient,
+  SignCommand,
+  SignCommandInput,
+  SignCommandOutput,
+  SigningAlgorithmSpec,
+} from "@aws-sdk/client-kms";
+
+import { getKmsConfig } from "../config/aws";
+
+export class KmsService {
+  constructor(
+    private readonly keyId: string,
+    private readonly signingAlgorithm: SigningAlgorithmSpec = "RSASSA_PKCS1_V1_5_SHA_256",
+    private readonly kmsClient: KMSClient = new KMSClient(getKmsConfig())
+  ) {}
+
+  async sign(message: string): Promise<string> {
+    const signCommandInput: SignCommandInput = {
+      Message: Buffer.from(message),
+      KeyId: this.keyId,
+      SigningAlgorithm: this.signingAlgorithm,
+      MessageType: "RAW",
+    };
+    const command: SignCommand = new SignCommand(signCommandInput);
+
+    let response: SignCommandOutput;
+    try {
+      response = await this.kmsClient.send(command);
+    } catch (error) {
+      console.log(`Error signing the access token: ${error as Error}`);
+      throw error;
+    }
+
+    if (!response.Signature) {
+      throw new Error("No signature returned");
+    }
+
+    return this.parseSignature(response.Signature);
+  }
+
+  private parseSignature(rawSignature: Uint8Array): string {
+    return Buffer.from(rawSignature).toString("base64url");
+  }
+
+  public async getPublicKey() {
+    const command: GetPublicKeyCommand = new GetPublicKeyCommand({
+      KeyId: this.keyId,
+    });
+
+    let response: GetPublicKeyResponse;
+    try {
+      response = await this.kmsClient.send(command);
+    } catch (error) {
+      console.log(`Error fetching public key: ${error as Error}`);
+      throw error;
+    }
+
+    const publicKey: Uint8Array = response.PublicKey as Uint8Array;
+    return this.parsePublicKey(publicKey);
+  }
+
+  private parsePublicKey(publicKeyRaw: Uint8Array) {
+    return Buffer.from(publicKeyRaw).toString("base64");
+  }
+}
---
+++ test/services/kmsService.test.ts
@@ -0,0 +1,47 @@
+import { mockClient } from "aws-sdk-client-mock";
+import { KMSClient, SignCommand } from "@aws-sdk/client-kms";
+import { KmsService } from "../../src/services/kmsService";
+
+const mockKmsClient = mockClient(KMSClient);
+
+let kmsService: KmsService;
+
+describe("kmsService.ts", () => {
+  beforeEach(() => {
+    kmsService = new KmsService(
+      "mock_key_id",
+      "RSASSA_PKCS1_V1_5_SHA_256",
+      new KMSClient()
+    );
+    mockKmsClient.reset();
+  });
+
+  it("should throw an error when an error happens when calling KMS", async () => {
+    mockKmsClient.on(SignCommand).rejects(new Error("SOME_KMS_ERROR"));
+    await expect(kmsService.sign("mock_message_to_sign")).rejects.toThrow(
+      "SOME_KMS_ERROR"
+    );
+  });
+
+  it("should throw an error when KMS response has no signature", async () => {
+    mockKmsClient.on(SignCommand).resolves({ Signature: undefined });
+    await expect(kmsService.sign("mock_message_to_sign")).rejects.toThrow(
+      "No signature returned"
+    );
+  });
+
+  it("should throw an error when KMS response has no signature", async () => {
+    mockKmsClient.on(SignCommand).resolves({ Signature: undefined });
+    await expect(kmsService.sign("mock_message_to_sign")).rejects.toThrow(
+      "No signature returned"
+    );
+  });
+
+  it("should return a base64 encoded signature when call to KMS is successful", async () => {
+    const mockSignature: Uint8Array = Buffer.from("mock_signature");
+    mockKmsClient.on(SignCommand).resolves({ Signature: mockSignature });
+    const response = await kmsService.sign("mock_message_to_sign");
+
+    expect(response).toEqual("bW9ja19zaWduYXR1cmU");
+  });
+});
