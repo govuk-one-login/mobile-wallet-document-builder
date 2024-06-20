75d550c994cee6742b9c86d7f7feeb421c532fbd
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-06-20 14:17:29 +0100

    feat(DCMAW-9342): implement logout endpoint

--- src/logout/controller.ts
+++ src/logout/controller.ts
@@ -1,21 +1,33 @@
 import {Request, Response} from "express";
 import {logger} from "../middleware/logger";
 
+const COOKIES_TO_DELETE = ["id_token", "access_token", "app"];
+
+export function deleteCookies(
+    req: Request,
+    res: Response,
+    cookieNames: string[]
+): void {
+    if (req.cookies) {
+        if (cookieNames) {
+            for (const cookieName of Object.keys(req.cookies)) {
+                if (cookieNames.includes(cookieName)) {
+                    res.clearCookie(cookieName);
+                }
+            }
+        }
+    }
+}
 export async function logoutPostController(
     req: Request,
     res: Response
 ): Promise<void> {
     try {
         const idToken = req.cookies.id_token;
-        console.log(idToken)
-        // await destroyUserSessions(
-        //     req,
-        //     req.session.user.subjectId,
-        //     req.app.locals.sessionStore
-        // );
+        deleteCookies(req, res, COOKIES_TO_DELETE);
         res.redirect(req.oidc.endSessionUrl({ id_token_hint: idToken }));
     } catch (error) {
-        logger.error(error, "An error happened trying to log out");
+        logger.error(error, "An error happened trying to logout");
         res.render("500.njk");
     }
 }
