e5f53bb322ce079dbe373520fda2c6e838b845a0
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2024-06-21 17:20:20 +0100

    test(DCMAW-9342): test new files

---
+++ test/logout/controller.test.ts
@@ -0,0 +1,36 @@
+import { getMockReq, getMockRes } from "@jest-mock/express";
+import {logoutGetController} from "../../src/logout/controller";
+import "../../src/types/Request";
+import * as utils from "../../src/logout/utils/deleteCookies";
+
+jest.mock(
+    "../../src/logout/utils/deleteCookies",
+    () => ({
+      deleteCookies: jest.fn(),
+    })
+);
+
+const deleteCookies =
+    utils.deleteCookies as jest.Mock;
+
+process.env.SELF = "http://localhost:8888";
+
+describe("controller.ts", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  it("should redirect the user to One Login to log out", async () => {
+    const req = getMockReq({
+      oidc: {endSessionUrl: jest.fn()},
+      cookies: {id_token: "id_token", access_token: "access_token", state: "state", app: "govuk-staging"}
+    });
+    const { res } = getMockRes({ cookies: {id_token: "id_token", access_token: "access_token", state: "state", app: "govuk-staging"}});
+
+    await logoutGetController(req, res);
+
+    expect(res.redirect).toHaveBeenCalled();
+    expect(req.oidc.endSessionUrl).toHaveBeenCalled();
+    expect(deleteCookies).toHaveBeenCalledWith(req, res, ["id_token", "access_token", "app"])
+  });
+});
