a007d855f4d51d5121f25a76f021f247decb92ad
Ana Hickmann <85736783+anahickmann@users.noreply.github.com>
2024-07-30 13:15:49 +0100

    Revert "Dcmaw 9706 revert"

--- src/appSelector/router.ts
+++ src/appSelector/router.ts
@@ -6,7 +6,7 @@ import {
 
 const router = express.Router();
 
-router.get("/select-app", appSelectorGetController);
-router.post("/select-app", appSelectorPostController);
+router.get("/select-app", appSelectorGetController());
+router.post("/select-app", appSelectorPostController());
 
 export { router as appSelectorRouter };
