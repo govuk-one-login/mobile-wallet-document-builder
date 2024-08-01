e1951b0f9d137b81a234a1edb37406c2bc976f65
Ana Hickmann <85736783+anahickmann@users.noreply.github.com>
2024-08-01 10:57:46 +0100

    Revert "Dcmaw 9706 revert" (#88)

--- src/appSelector/controller.ts
+++ src/appSelector/controller.ts
@@ -2,42 +2,59 @@ import { Request, Response } from "express";
 import { logger } from "../middleware/logger";
 import { getCookieExpiry } from "../utils/getCookieExpiry";
 import { isAuthenticated } from "../utils/isAuthenticated";
+import { ExpressRouteFunction } from "../types/ExpressRouteFunction";
+import { getEnvironment } from "../config/appConfig";
+import { nonStagingApps, stagingApps } from "./views/apps";
 
-export async function appSelectorGetController(
-  req: Request,
-  res: Response
-): Promise<void> {
-  try {
-    res.render("select-app-form.njk", {
-      authenticated: isAuthenticated(req),
-    });
-  } catch (error) {
-    logger.error(error, "An error happened rendering app selection page");
-    res.render("500.njk");
-  }
+export interface AppSelectorConfig {
+  environment: string;
+  cookieExpiry?: number;
 }
 
-export async function appSelectorPostController(
-  req: Request,
-  res: Response
-): Promise<void> {
-  try {
-    const selectedApp = req.body["select-app-choice"];
-
-    if (selectedApp) {
-      res.cookie("app", selectedApp, {
-        httpOnly: true,
-        maxAge: getCookieExpiry(),
-      });
-      res.redirect(`/select-document`);
-    } else {
+export function appSelectorGetController(
+  config: AppSelectorConfig = {
+    environment: getEnvironment(),
+  }
+): ExpressRouteFunction {
+  return function (req: Request, res: Response): void {
+    try {
       res.render("select-app-form.njk", {
-        isInvalid: selectedApp === undefined,
+        apps: config.environment === "staging" ? stagingApps : nonStagingApps,
         authenticated: isAuthenticated(req),
       });
+    } catch (error) {
+      logger.error(error, "An error happened rendering app selection page");
+      res.render("500.njk");
     }
-  } catch (error) {
-    logger.error(error, "An error happened selecting app");
-    res.render("500.njk");
+  };
+}
+
+export function appSelectorPostController(
+  config: AppSelectorConfig = {
+    environment: getEnvironment(),
+    cookieExpiry: getCookieExpiry(),
   }
+): ExpressRouteFunction {
+  return function (req: Request, res: Response): void {
+    try {
+      const selectedApp = req.body["select-app-choice"];
+
+      if (selectedApp) {
+        res.cookie("app", selectedApp, {
+          httpOnly: true,
+          maxAge: config.cookieExpiry,
+        });
+        res.redirect(`/select-document`);
+      } else {
+        res.render("select-app-form.njk", {
+          isInvalid: selectedApp === undefined,
+          apps: config.environment === "staging" ? stagingApps : nonStagingApps,
+          authenticated: isAuthenticated(req),
+        });
+      }
+    } catch (error) {
+      logger.error(error, "An error happened selecting app");
+      res.render("500.njk");
+    }
+  };
 }
