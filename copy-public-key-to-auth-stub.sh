af3a95cb325b5ddb1fe938f9dfb8f35b61038c48
Jamie Lutton <jamie.lutton@digital.cabinet-office.gov.uk>
2024-11-15 14:35:46 +0000

    DCMAW-10509: Update script to configure auth stub

--- copy-public-key-to-auth-stub.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -eu
-
-aws kms get-public-key --endpoint-url=http://localhost:4561 --region eu-west-2 --key-id alias/localClientSigningKeyAlias --output text --query PublicKey | base64 --decode > LocalClientSigningKey.der
-openssl rsa -pubin -inform DER -outform PEM -in LocalClientSigningKey.der -pubout -out ../mobile-platform-back/auth-stub/src/client-keys/dev/document-builder-local.pem
