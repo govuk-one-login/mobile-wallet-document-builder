4149aeaeeae28c565a069ba8892b1e55bf474b6d
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-01-16 16:51:39 +0000

    feat: create credentialSubject for veteran card

--- src/veteranCardDocumentBuilder/models/veteranCardDocument.ts
+++ src/veteranCardDocumentBuilder/models/veteranCardDocument.ts
@@ -1,13 +1,15 @@
 import { VeteranCardInputData } from "../types/VeteranCardInputData";
 import { getNameParts } from "../../utils/getNameParts";
-import { CredentialType } from "../../types/CredentialType";
 import { CredentialSubject } from "../../types/CredentialSubject";
+import { VeteranCredentialSubject } from "../types/VeteranCredentialSubject";
+import { base64Photo } from "../base64Photo";
+import { CredentialType } from "../../types/CredentialType";
 
 export class VeteranCardDocument {
   public readonly type: string[];
   public readonly credentialSubject: CredentialSubject;
 
-  constructor(type: string[], credentialSubject: CredentialSubject) {
+  constructor(type: string[], credentialSubject: VeteranCredentialSubject) {
     this.type = type;
     this.credentialSubject = credentialSubject;
   }
@@ -26,15 +28,50 @@ export class VeteranCardDocument {
     this.trimRequestBody(input);
 
     const type = ["VerifiableCredential", credentialType];
-    const credentialSubject: CredentialSubject = {
+    const credentialSubject: VeteranCredentialSubject = {
       name: [
         {
           nameParts: getNameParts(input.givenName, input.familyName),
         },
       ],
+      birthDate: [
+        {
+          value: this.getFormattedDate(
+            input["dateOfBirth-year"],
+            input["dateOfBirth-month"],
+            input["dateOfBirth-day"]
+          ),
+        },
+      ],
+      veteranCard: [
+        {
+          expiryDate: this.getFormattedDate(
+            input["cardExpiryDate-year"],
+            input["cardExpiryDate-month"],
+            input["cardExpiryDate-day"]
+          ),
+          serviceStart: this.getFormattedDate(
+            input["serviceStartDate-year"],
+            input["serviceStartDate-month"],
+            input["serviceStartDate-day"]
+          ),
+          serviceEnd: this.getFormattedDate(
+            input["serviceEndDate-year"],
+            input["serviceEndDate-month"],
+            input["serviceEndDate-day"]
+          ),
+          serviceNumber: input.serviceNumber,
+          serviceBranch: input.serviceBranch,
+          photo: base64Photo,
+        },
+      ],
     };
 
     return new VeteranCardDocument(type, credentialSubject);
+  }
+
+  private static getFormattedDate(year: string, month: string, day: string) {
+    return `${year}-${month}-${day}`;
   }
 
   private static trimRequestBody(input: VeteranCardInputData) {
