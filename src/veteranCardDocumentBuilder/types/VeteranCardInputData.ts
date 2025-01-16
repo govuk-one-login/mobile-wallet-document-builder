6eeb6ea3da1fd2d6b190c64da003ad48c256dc48
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-01-16 16:50:36 +0000

    feat: update veteran card input data type

--- src/veteranCardDocumentBuilder/types/VeteranCardInputData.ts
+++ src/veteranCardDocumentBuilder/types/VeteranCardInputData.ts
@@ -1,4 +1,19 @@
 export interface VeteranCardInputData {
-  givenName?: string;
-  familyName?: string;
+  givenName: string;
+  familyName: string;
+  "dateOfBirth-day": string;
+  "dateOfBirth-month": string;
+  "dateOfBirth-year": string;
+  "cardExpiryDate-day": string;
+  "cardExpiryDate-month": string;
+  "cardExpiryDate-year": string;
+  "serviceStartDate-day": string;
+  "serviceStartDate-month": string;
+  "serviceStartDate-year": string;
+  "serviceEndDate-day": string;
+  "serviceEndDate-month": string;
+  "serviceEndDate-year": string;
+  serviceNumber: string;
+  serviceBranch: string;
+  throwError: string;
 }
