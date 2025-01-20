b9457146853a794f170559132416ddcdc06b04d5
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-01-20 17:56:26 +0000

    style: format unit tests for s3 service

--- test/services/s3Service.test.ts
+++ test/services/s3Service.test.ts
@@ -1,88 +1,95 @@
-import {saveDocument} from "../../src/services/databaseService";
+import { saveDocument } from "../../src/services/databaseService";
 
 process.env.PHOTOS_BUCKET_NAME = "photosBucket";
 process.env.ENVIRONMENT = "local";
 import { mockClient } from "aws-sdk-client-mock";
 import {
-    S3Client,
-    PutObjectCommand,
-    GetObjectCommand, GetObjectCommandOutput, NoSuchKey,
+  S3Client,
+  PutObjectCommand,
+  GetObjectCommand,
+  GetObjectCommandOutput,
+  NoSuchKey,
 } from "@aws-sdk/client-s3";
 import { uploadPhoto, getPhoto } from "../../src/services/s3Service";
 import "aws-sdk-client-mock-jest";
 
 describe("s3Service.ts", () => {
+  const bucketName = process.env.PHOTOS_BUCKET_NAME!; //(non-null assertion operator) TO BE REMOVED
+  const imageName = "fileName";
+  const imageBuffer = Buffer.alloc(10);
 
-    const bucketName = process.env.PHOTOS_BUCKET_NAME!; //(non-null assertion operator) TO BE REMOVED
-    const imageName = "fileName";
-    const imageBuffer = Buffer.alloc(10);
-
-    describe("Upload Photo", () => {
-
+  describe("Upload Photo", () => {
     it("should save a photo to the s3 bucket", async () => {
+      const s3Mock = mockClient(S3Client);
+      s3Mock.on(PutObjectCommand).resolvesOnce({
+        $metadata: {
+          httpStatusCode: 200,
+        },
+      });
 
-        const s3Mock = mockClient(S3Client);
-        s3Mock.on(PutObjectCommand).resolvesOnce({
-            $metadata: {
-                httpStatusCode: 200,
-            },
-        });
+      await expect(
+        uploadPhoto(imageBuffer, imageName, bucketName)
+      ).resolves.not.toThrow();
+      expect(s3Mock).toHaveReceivedCommandWith(PutObjectCommand, {
+        Bucket: bucketName,
+        Key: imageName,
+        Body: imageBuffer,
+      });
+    });
+    it("should throw an error caught when trying to save a photo to the s3 bucket", async () => {
+      const s3Mock = mockClient(S3Client);
+      s3Mock.on(PutObjectCommand).rejectsOnce("SOME_S3_ERROR");
 
-        await expect(
-            uploadPhoto(imageBuffer, imageName, bucketName)
-        ).resolves.not.toThrow();
-        expect(s3Mock).toHaveReceivedCommandWith(PutObjectCommand, {Bucket: bucketName, Key: imageName, Body: imageBuffer,});
-    })
-        it("should throw an error caught when trying to save a photo to the s3 bucket", async () => {
-            const s3Mock = mockClient(S3Client);
-            s3Mock.on(PutObjectCommand).rejectsOnce("SOME_S3_ERROR")
+      await expect(
+        uploadPhoto(imageBuffer, imageName, bucketName)
+      ).rejects.toThrow("SOME_S3_ERROR");
+      expect(s3Mock).toHaveReceivedCommandWith(PutObjectCommand, {
+        Bucket: bucketName,
+        Key: imageName,
+        Body: imageBuffer,
+      });
+    });
+  });
+  describe("Get Photo", () => {
+    const mockS3Response = (content: string) => {
+      return {
+        transformToString: async () => content,
+      };
+    };
 
-            await expect(
-                uploadPhoto(imageBuffer, imageName, bucketName)
-            ).rejects.toThrow("SOME_S3_ERROR");
-            expect(s3Mock).toHaveReceivedCommandWith(PutObjectCommand, {Bucket: bucketName, Key: imageName, Body: imageBuffer,});
+    it("should get a photo from the s3 bucket", async () => {
+      const s3Mock = mockClient(S3Client);
+      s3Mock
+        .on(GetObjectCommand)
+        .resolvesOnce({
+          Body: mockS3Response("base64image"),
+        } as GetObjectCommandOutput);
 
-        })
-  })
-    describe("Get Photo", () => {
+      const response = await getPhoto(imageName, bucketName);
+      expect(response).toEqual("base64image");
+    });
 
-        const mockS3Response = (content: string) => {
-            return {
-                transformToString: async () => content,
-            };
-        };
+    it("it should return undefined if the S3 response Body is undefined", async () => {
+      const s3Mock = mockClient(S3Client);
+      s3Mock
+        .on(GetObjectCommand)
+        .resolvesOnce({ Body: undefined } as GetObjectCommandOutput);
 
-        it("should get a photo from the s3 bucket", async () => {
+      const response = await getPhoto(imageName, bucketName);
+      expect(response).toEqual(undefined);
+    });
 
-            const s3Mock = mockClient(S3Client);
-            s3Mock.on(GetObjectCommand).resolvesOnce({Body: mockS3Response("base64image")
-            } as GetObjectCommandOutput);
+    it("it should throw the error thrown by the S3 client", async () => {
+      const s3Mock = mockClient(S3Client);
+      s3Mock.on(GetObjectCommand).rejectsOnce("Error getting image");
 
-            const response = await getPhoto(imageName, bucketName);
-            expect(response).toEqual("base64image");
-
-        })
-
-        it("it should return undefined if the S3 response Body is undefined", async () => {
-            const s3Mock = mockClient(S3Client);
-            s3Mock.on(GetObjectCommand).resolvesOnce({Body: undefined
-            } as GetObjectCommandOutput);
-
-            const response = await getPhoto(imageName, bucketName);
-            expect(response).toEqual(undefined);
-        })
-
-        it("it should throw the error thrown by the S3 client", async () => {
-            const s3Mock = mockClient(S3Client);
-            s3Mock.on(GetObjectCommand).rejectsOnce("Error getting image");
-
-            await expect(
-                getPhoto(imageName, bucketName)
-            ).rejects.toThrow("Error getting image");
-            expect(s3Mock).toHaveReceivedCommandWith(GetObjectCommand, {Bucket: bucketName, Key: imageName});
-
-        })
-
-    })
-
-})
+      await expect(getPhoto(imageName, bucketName)).rejects.toThrow(
+        "Error getting image"
+      );
+      expect(s3Mock).toHaveReceivedCommandWith(GetObjectCommand, {
+        Bucket: bucketName,
+        Key: imageName,
+      });
+    });
+  });
+});
