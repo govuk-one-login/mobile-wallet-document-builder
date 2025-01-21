20c91ec7616e28e1e27f7d3ec7e2ba10b94150e8
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-01-21 19:10:39 +0000

    feat: fetch photo from s3 and add it to document

--- src/document/controller.ts
+++ src/document/controller.ts
@@ -3,6 +3,10 @@ import { getDocument } from "../services/databaseService";
 import { logger } from "../middleware/logger";
 import { NinoDocument } from "../ninoDocumentBuilder/models/ninoDocument";
 import { DbsDocument } from "../dbsDocumentBuilder/models/dbsDocument";
+import { VeteranCardDocument } from "../veteranCardDocumentBuilder/models/veteranCardDocument";
+import { getPhoto } from "../services/s3Service";
+import { getPhotosBucketName } from "../config/appConfig";
+import { CredentialType } from "../types/CredentialType";
 
 export async function documentController(
   req: Request,
@@ -15,13 +19,28 @@ export async function documentController(
     if (!databaseItem) {
       return res.status(204).send();
     }
-
     const documentString = databaseItem.vc as string;
-    const document: NinoDocument | DbsDocument = JSON.parse(documentString);
+    const document: NinoDocument | DbsDocument | VeteranCardDocument =
+      JSON.parse(documentString);
+
+    if (isDigitalVeteranCard(document)) {
+      const photo = await getPhoto(documentId, getPhotosBucketName());
+      if (!photo) {
+        return res.status(204).send();
+      }
+      (document as VeteranCardDocument).credentialSubject.veteranCard[0].photo =
+        photo;
+    }
 
     return res.status(200).json(document);
   } catch (error) {
     logger.error(error, "An error happened processing request to get document");
     return res.status(500).send();
   }
+}
+
+function isDigitalVeteranCard(
+  document: NinoDocument | DbsDocument | VeteranCardDocument
+) {
+  return document.type.includes(CredentialType.digitalVeteranCard);
 }
