28947f02e99d4cbac2f2c7d8a064099adfe3bf25
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-05-02 00:00:15 +0100

    test: Add more unit test and refactor drivingPrivilege builder

--- src/utils/drivingPrivilegeBuilder.ts
+++ src/utils/drivingPrivilegeBuilder.ts
@@ -1,13 +1,9 @@
 import { DrivingPrivilege } from "../mdlDocumentBuilder/types/DrivingPrivilege";
-import { formatDate } from "./dateValidator";
 import {MdlRequestBody} from "../mdlDocumentBuilder/types/MdlRequestBody";
-import {logger} from "../middleware/logger";
 
 
 export function buildDrivingPrivileges(body: MdlRequestBody, numPrivileges: number): DrivingPrivilege[] {
     const drivingPrivileges: DrivingPrivilege[] = [];
-    let privilege: DrivingPrivilege;
-
 
     const issueDay = body["fullPrivilegeIssue-day"] ? body["fullPrivilegeIssue-day"] : [""];
     const issueMonth = body["fullPrivilegeIssue-month"] ? body["fullPrivilegeIssue-month"] : [""];
@@ -16,39 +12,22 @@ export function buildDrivingPrivileges(body: MdlRequestBody, numPrivileges: numb
     const expiryMonth = body["fullPrivilegeExpiry-month"] ? body["fullPrivilegeExpiry-month"] : [""];
     const expiryYear = body["fullPrivilegeExpiry-year"] ? body["fullPrivilegeExpiry-year"] : [""];
 
-    if (numPrivileges === 1) {
+    for (let i=0; i<numPrivileges; i++ ) {
 
-        let issueDate = issueDay + "-" + issueMonth + "-" + issueYear;
+        let issueDate = `${issueDay[i] || ""}-${issueMonth[i] || ""}-${issueYear[i] || ""}`;
         if (issueDate === "--") issueDate = "";
-        let expiryDate = expiryDay + "-" + expiryMonth + "-" + expiryYear;
+
+        let expiryDate = `${expiryDay[i] || ""}-${expiryMonth[i] || ""}-${expiryYear[i] || ""}`;
         if (expiryDate === "--") expiryDate = "";
 
-        privilege = {
-            vehicle_category_code: body["vehicleCategoryCode"][0],
+        const privilege: DrivingPrivilege = {
+            vehicle_category_code: body["vehicleCategoryCode"][i],
             issue_date: issueDate,
             expiry_date: expiryDate,
         };
+
         drivingPrivileges.push(privilege);
-    } else {
-
-        for (let i=0; i<numPrivileges; i++ ) {
-
-            let issueDate = `${issueDay[i] || ""}-${issueMonth[i] || ""}-${issueYear[i] || ""}`;
-            if (issueDate === "--") issueDate = "";
-
-            let expiryDate = `${expiryDay[i] || ""}-${expiryMonth[i] || ""}-${expiryYear[i] || ""}`;
-            if (expiryDate === "--") expiryDate = "";
-
-            const privilege: DrivingPrivilege = {
-                vehicle_category_code: body["vehicleCategoryCode"][i],
-                issue_date: issueDate,
-                expiry_date: expiryDate,
-            };
-
-            drivingPrivileges.push(privilege);
-        }
     }
 
     return drivingPrivileges;
 }
-
--- test/utils/drivingPrivilegeBuilder.test.ts
+++ test/utils/drivingPrivilegeBuilder.test.ts
@@ -1,18 +1,8 @@
-import { formatDate } from "../../src/utils/dateValidator";
 import {buildDrivingPrivileges} from "../../src/utils/drivingPrivilegeBuilder";
 import {buildMdlRequestBody} from "./mdlRequestBodyBuilder";
 
-jest.mock('../../src/utils/dateValidator', () => ({
-    formatDate: jest.fn(),
-}));
-
 describe("buildDrivingPrivilege", () => {
-    beforeEach(() => {
-        (formatDate as jest.Mock).mockReset();
-    });
-
     it("should build a single driving privilege when numPrivileges is 1", () => {
-
         const body = buildMdlRequestBody(
             {
                 vehicleCategoryCode: ["C"],
@@ -27,7 +17,7 @@ describe("buildDrivingPrivilege", () => {
 
         const numPrivileges = 1;
         const result = buildDrivingPrivileges(body, numPrivileges);
-        expect(result).toHaveLength(1);
+
         expect(result[0]).toEqual({
                 vehicle_category_code: "C",
                 issue_date: "01-05-2025",
@@ -37,7 +27,6 @@ describe("buildDrivingPrivilege", () => {
     });
 
     it("should build multiple driving privileges when numPrivileges is greater than 1", () => {
-
         const body = buildMdlRequestBody(
             {
                 vehicleCategoryCode: ["A", "B"],
@@ -49,6 +38,7 @@ describe("buildDrivingPrivilege", () => {
                 "fullPrivilegeExpiry-year": ["", "2030"]
             }
         );
+
         const numPrivileges = 2;
         const result = buildDrivingPrivileges(body, numPrivileges);
 
@@ -62,7 +52,42 @@ describe("buildDrivingPrivilege", () => {
                 vehicle_category_code: "B",
                 issue_date: "01-05-2025",
                 expiry_date: "10-08-2030",
+            },
+        ]);
+    });
+
+    it("should build multiple driving privileges when numPrivileges is greater than 1 and issue and expiry dates are empty", () => {
+        const body = buildMdlRequestBody(
+            {
+                vehicleCategoryCode: ["A", "B", "C"],
+                "fullPrivilegeIssue-day": ["", "", ""],
+                "fullPrivilegeIssue-month": ["", "", ""],
+                "fullPrivilegeIssue-year": ["", "", ""],
+                "fullPrivilegeExpiry-day": ["", "", ""],
+                "fullPrivilegeExpiry-month": ["", "", ""],
+                "fullPrivilegeExpiry-year": ["", "", ""]
             }
-        ])
+        );
+
+        const numPrivileges = 3;
+        const result = buildDrivingPrivileges(body, numPrivileges);
+
+        expect(result).toEqual([
+            {
+                vehicle_category_code: "A",
+                issue_date: "",
+                expiry_date: "",
+            },
+            {
+                vehicle_category_code: "B",
+                issue_date: "",
+                expiry_date: "",
+            },
+            {
+                vehicle_category_code: "C",
+                issue_date: "",
+                expiry_date: "",
+            },
+        ]);
     })
 })
