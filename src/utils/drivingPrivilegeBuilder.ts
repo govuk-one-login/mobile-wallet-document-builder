c2d627413b43404e26a754e7827d9be45b89cb7f
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-05-14 12:24:34 +0100

    Remove code smells and reformat code (#239)
    * chore: remove non-null expression
    
    * style: reformat code

--- src/utils/drivingPrivilegeBuilder.ts
+++ src/utils/drivingPrivilegeBuilder.ts
@@ -1,32 +1,32 @@
-import {MdlRequestBody} from "../mdlDocumentBuilder/types/MdlRequestBody";
-import {DrivingPrivilege} from "../mdlDocumentBuilder/types/DrivingPrivilege";
-import {formatDate} from "./dateValidator";
+import { MdlRequestBody } from "../mdlDocumentBuilder/types/MdlRequestBody";
+import { DrivingPrivilege } from "../mdlDocumentBuilder/types/DrivingPrivilege";
+import { formatDate } from "./dateValidator";
 
 export function buildDrivingPrivileges(
-    body: MdlRequestBody,
+  body: MdlRequestBody,
 ): DrivingPrivilege[] {
   if (!body.vehicleCategoryCode) {
     return [];
   }
   const drivingPrivileges: DrivingPrivilege[] = [];
-  const issueDay = stringToArray(body["fullPrivilegeIssue-day"]!);
-  const issueMonth = stringToArray(body["fullPrivilegeIssue-month"]!);
-  const issueYear = stringToArray(body["fullPrivilegeIssue-year"]!);
-  const expiryDay = stringToArray(body["fullPrivilegeExpiry-day"]!);
-  const expiryMonth = stringToArray(body["fullPrivilegeExpiry-month"]!);
-  const expiryYear = stringToArray(body["fullPrivilegeExpiry-year"]!);
+  const issueDay = stringToArray(body["fullPrivilegeIssue-day"]);
+  const issueMonth = stringToArray(body["fullPrivilegeIssue-month"]);
+  const issueYear = stringToArray(body["fullPrivilegeIssue-year"]);
+  const expiryDay = stringToArray(body["fullPrivilegeExpiry-day"]);
+  const expiryMonth = stringToArray(body["fullPrivilegeExpiry-month"]);
+  const expiryYear = stringToArray(body["fullPrivilegeExpiry-year"]);
   const numberOfPrivileges = stringToArray(body.vehicleCategoryCode).length;
   const vehicleCategoryCode = stringToArray(body.vehicleCategoryCode);
 
   for (let i = 0; i < numberOfPrivileges; i++) {
     const issueDate =
-        issueDay[i] === "" || issueMonth[i] === "" || issueYear[i] === ""
-            ? null
-            : formatDate(issueDay[i], issueMonth[i], issueYear[i]);
+      issueDay[i] === "" || issueMonth[i] === "" || issueYear[i] === ""
+        ? null
+        : formatDate(issueDay[i], issueMonth[i], issueYear[i]);
     const expiryDate =
-        expiryDay[i] === "" || expiryMonth[i] === "" || expiryYear[i] === ""
-            ? null
-            : formatDate(expiryDay[i], expiryMonth[i], expiryYear[i]);
+      expiryDay[i] === "" || expiryMonth[i] === "" || expiryYear[i] === ""
+        ? null
+        : formatDate(expiryDay[i], expiryMonth[i], expiryYear[i]);
 
     const privilege: DrivingPrivilege = {
       vehicle_category_code: vehicleCategoryCode[i],
@@ -39,4 +39,5 @@ export function buildDrivingPrivileges(
   return drivingPrivileges;
 }
 
-const stringToArray = (input: string | string[]): string[] => Array.isArray(input) ? input : [input];
+const stringToArray = (input: string | string[]): string[] =>
+  Array.isArray(input) ? input : [input];
