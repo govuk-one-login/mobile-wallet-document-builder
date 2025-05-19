5c78be71a271ba1ab139144256f5d5c42e2d089d
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-05-19 11:05:31 +0100

    feat: add interface to buildDrivingPrivileges function

--- src/mdlDocumentBuilder/helpers/drivingPrivilegeBuilder.ts
+++ src/mdlDocumentBuilder/helpers/drivingPrivilegeBuilder.ts
@@ -11,15 +11,15 @@ export function getFullDrivingPrivileges(body: MdlRequestBody) {
   const fullExpiryYears = stringToArray(body["fullPrivilegeExpiry-year"]);
   const fullVehicleCategoryCodes = stringToArray(body.fullVehicleCategoryCode);
 
-  return buildDrivingPrivileges(
-    fullIssueDays,
-    fullIssueMonths,
-    fullIssueYears,
-    fullExpiryDays,
-    fullExpiryMonths,
-    fullExpiryYears,
-    fullVehicleCategoryCodes,
-  );
+  return buildDrivingPrivileges({
+    vehicleCategoryCodes: fullVehicleCategoryCodes,
+    issueDays: fullIssueDays,
+    issueMonths: fullIssueMonths,
+    issueYears: fullIssueYears,
+    expiryDays: fullExpiryDays,
+    expiryMonths: fullExpiryMonths,
+    expiryYears: fullExpiryYears,
+  });
 }
 
 export function getProvisionalDrivingPrivileges(body: MdlRequestBody) {
@@ -49,29 +49,39 @@ export function getProvisionalDrivingPrivileges(body: MdlRequestBody) {
     body["provisionalPrivilegeExpiry-year"]!,
   );
 
-  return buildDrivingPrivileges(
-    provisionalVehicleCategoryCodes,
-    provisionalIssueDays,
-    provisionalIssueMonths,
-    provisionalIssueYears,
-    provisionalExpiryDays,
-    provisionalExpiryMonths,
-    provisionalExpiryYears,
-  );
+  return buildDrivingPrivileges({
+    vehicleCategoryCodes: provisionalVehicleCategoryCodes,
+    issueDays: provisionalIssueDays,
+    issueMonths: provisionalIssueMonths,
+    issueYears: provisionalIssueYears,
+    expiryDays: provisionalExpiryDays,
+    expiryMonths: provisionalExpiryMonths,
+    expiryYears: provisionalExpiryYears,
+  });
 }
 
 export const stringToArray = (input: string | string[]): string[] =>
   Array.isArray(input) ? input : [input];
 
-export function buildDrivingPrivileges(
-  issueDays: string[],
-  issueMonths: string[],
-  issueYears: string[],
-  expiryDays: string[],
-  expiryMonths: string[],
-  expiryYears: string[],
-  vehicleCategoryCodes: string[],
-): DrivingPrivilege[] {
+interface BuildDrivingPrivilegesParams {
+  vehicleCategoryCodes: string[];
+  issueDays: string[];
+  issueMonths: string[];
+  issueYears: string[];
+  expiryDays: string[];
+  expiryMonths: string[];
+  expiryYears: string[];
+}
+
+export function buildDrivingPrivileges({
+  vehicleCategoryCodes,
+  issueDays,
+  issueMonths,
+  issueYears,
+  expiryDays,
+  expiryMonths,
+  expiryYears,
+}: BuildDrivingPrivilegesParams): DrivingPrivilege[] {
   const drivingPrivileges: DrivingPrivilege[] = [];
 
   for (let i = 0; i < vehicleCategoryCodes.length; i++) {
