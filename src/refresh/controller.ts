d64bf3b6a40866257a65ff99130accc5904219b9
Shirin Ebrahimi <shirin.ebrahimi@digital.cabinet-office.gov.uk>
2025-10-16 19:57:40 +0100

    feat: add refreshPostController

--- src/refresh/controller.ts
+++ src/refresh/controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from "express";
 import { isAuthenticated } from "../utils/isAuthenticated";
 import { logger } from "../middleware/logger";
+import { CredentialType } from "../types/CredentialType";
 
 export async function refreshGetController(
   req: Request,
@@ -10,10 +11,42 @@ export async function refreshGetController(
     const { credentialType } = req.params;
     res.render("refresh-form.njk", {
       authenticated: isAuthenticated(req),
-      credentialType: { credentialType },
+      credentialType,
     });
   } catch (error) {
     logger.error(error, "An error happened rendering Refresh Credential page");
     res.render("500.njk");
   }
 }
+
+export async function refreshPostController(
+  req: Request<{ credentialType: CredentialType }>,
+  res: Response,
+): Promise<void> {
+  try {
+    const { credentialType } = req.params;
+    const decision = req.body.refreshChoice;
+
+    if (decision === "No") {
+      res.render("no-update.njk", {
+        authenticated: isAuthenticated(req),
+        credentialType,
+      });
+      return;
+    }
+    res.render(templateByCredential[credentialType], {
+      authenticated: isAuthenticated(req),
+      credentialType,
+    });
+  } catch (error) {
+    logger.error(error, "An error happened processing refresh credential");
+    res.render("500.njk");
+  }
+}
+
+const templateByCredential: Record<CredentialType, string> = {
+  [CredentialType.SocialSecurityCredential]: "nino-document-details-form.njk",
+  [CredentialType.BasicDisclosureCredential]: "dbs-document-details-form.njk",
+  [CredentialType.DigitalVeteranCard]: "veteran-card-document-details-form.njk",
+  [CredentialType.MobileDrivingLicence]: "mdl-document-details-form.njk",
+};
--- src/refresh/router.ts
+++ src/refresh/router.ts
@@ -1,9 +1,10 @@
 import express from "express";
 import { requiresAuth } from "../middleware/requiresAuth";
-import { refreshGetController } from "./controller";
+import { refreshGetController, refreshPostController } from "./controller";
 
 const router = express.Router();
 
 router.get("/refresh/:credentialType", requiresAuth, refreshGetController);
+router.post("/refresh/:credentialType", requiresAuth, refreshPostController);
 
 export { router as refreshRouter };
