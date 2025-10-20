dd286d4249e53694e0dfba01527df8055ded272a
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-20 19:25:04 +0100

    feat: remove async and try-catch block from refresh controller

--- src/refresh/controller.ts
+++ src/refresh/controller.ts
@@ -1,43 +1,25 @@
 import { Request, Response } from "express";
-import { logger } from "../middleware/logger";
 import { CredentialType } from "../types/CredentialType";
 
-export async function refreshGetController(
-  req: Request,
-  res: Response,
-): Promise<void> {
-  try {
-    const { credentialType } = req.params;
-    res.render("refresh-form.njk", {
-      credentialType,
-    });
-  } catch (error) {
-    logger.error(error, "An error happened rendering Refresh Credential page");
-    res.render("500.njk");
-  }
+export function refreshGetController(req: Request, res: Response): void {
+  const { credentialType } = req.params;
+  res.render("refresh-form.njk", {
+    credentialType,
+  });
 }
 
-export async function refreshPostController(
-  req: Request,
-  res: Response,
-): Promise<void> {
-  try {
-    const { credentialType } = req.params;
-    const refreshChoice = req.body.refreshChoice;
-
-    if (refreshChoice === "No") {
-      return res.redirect("/no-update");
-    }
-
-    if (
-      !Object.values(CredentialType).includes(credentialType as CredentialType)
-    ) {
-      throw new Error(`Unknown credential type: ${credentialType}`);
-    }
-
-    res.redirect(`/select-app/?credentialType=${credentialType}`);
-  } catch (error) {
-    logger.error(error, "An error happened processing refresh credential");
-    res.render("500.njk");
+export function refreshPostController(req: Request, res: Response): void {
+  const refreshChoice = req.body.refreshChoice;
+  if (refreshChoice === "No") {
+    return res.redirect("/no-update");
   }
+
+  const { credentialType } = req.params;
+  if (
+    !Object.values(CredentialType).includes(credentialType as CredentialType)
+  ) {
+    throw new Error(`Unknown credential type: ${credentialType}`);
+  }
+
+  res.redirect(`/select-app/?credentialType=${credentialType}`);
 }
