48cfbb687b25c6ad10e9e3cdb3a11cda0715aead
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-24 15:22:28 +0100

    test: update credentialOfferViewer tests

--- test/credentialOfferViewer/controller.test.ts
+++ test/credentialOfferViewer/controller.test.ts
@@ -1,115 +1,78 @@
-import {
-  CredentialOfferViewerConfig,
-  credentialOfferViewerController,
-} from "../../src/credentialOfferViewer/controller";
-import * as credentialOfferService from "../../src/credentialOfferViewer/services/credentialOfferService";
-import * as customCredentialOfferUrl from "../../src/credentialOfferViewer/helpers/customCredentialOfferUrl";
+import QRCode from "qrcode";
 import { getMockReq, getMockRes } from "@jest-mock/express";
-import { WalletAppsConfig } from "../../src/config/walletAppsConfig";
+import { credentialOfferViewerController } from "../../src/credentialOfferViewer/controller";
+import { getCredentialOfferUrl } from "../../src/credentialOfferViewer/services/credentialOfferService";
+import { customiseCredentialOfferUrl } from "../../src/credentialOfferViewer/helpers/customCredentialOfferUrl";
+import type { Request, Response } from "express";
 
-jest.mock(
-  "../../src/credentialOfferViewer/services/credentialOfferService",
-  () => ({
-    getCredentialOfferUrl: jest.fn(),
-  }),
-);
-jest.mock(
-  "../../src/credentialOfferViewer/helpers/customCredentialOfferUrl",
-  () => ({
-    customiseCredentialOfferUrl: jest.fn(),
-  }),
-);
-jest.mock("qrcode", () => ({
-  toDataURL: () => "data:image/png;base64,iVBORw0KGgoAAAANSU",
-}));
+jest.mock("../../src/credentialOfferViewer/services/credentialOfferService");
+jest.mock("../../src/credentialOfferViewer/helpers/customCredentialOfferUrl");
+jest.mock("qrcode");
 
-const WALLET_SUBJECT_ID =
+const wallletSubjectId =
   "urn:fdc:wallet.account.gov.uk:2024:DtPT8x-dp_73tnlY3KNTiCitziN9GEherD16bqxNt9i";
 
-const walletAppsConfig: WalletAppsConfig = {
-  "test-app-1": {
-    url: "https://test-one.com/wallet/",
-    name: "Test App (1)",
-  },
-  "test-app-2": {
-    url: "https://test-two.com/wallet/",
-    name: "Test App (2)",
-  },
-  "test-app-3": {
-    url: "https://test-three.com/wallet/",
-    name: "Test App (3)",
-  },
+const walletAppsConfig = {
+  "test-app-1": { url: "https://test-one.com", name: "Test App (1)" },
+  "test-app-2": { url: "https://test-two.com", name: "Test App (2)" },
 };
 
-const config: CredentialOfferViewerConfig = {
-  walletAppsConfig: walletAppsConfig,
-  walletApps: ["test-app-1", "test-app-3"],
+const config = {
+  walletAppsConfig,
+  walletApps: ["test-app-1", "test-app-2"],
   environment: "test",
 };
 
-describe("credentialOfferViewerControllers", () => {
-  afterEach(() => {
+describe("credentialOfferViewerController", () => {
+  let req: Request;
+  let res: Response;
+
+  beforeEach(() => {
     jest.clearAllMocks();
-  });
 
-  const getCredentialOfferUrl =
-    credentialOfferService.getCredentialOfferUrl as jest.Mock;
-  const customiseCredentialOfferUrl =
-    customCredentialOfferUrl.customiseCredentialOfferUrl as jest.Mock;
+    req = getMockReq({
+      params: { itemId: "2e0fac05-4b38-480f-9cbd-b046eabe1e46" },
+      cookies: { app: "test-app-1", wallet_subject_id: wallletSubjectId },
+      query: { type: "BasicCheckCredential", error: "" },
+    });
+    res = getMockRes().res;
 
-  const req = getMockReq({
-    params: {
-      itemId: "2e0fac05-4b38-480f-9cbd-b046eabe1e46",
-    },
-    cookies: {
-      app: "test-app-3",
-      id_token: "id_token",
-      wallet_subject_id: WALLET_SUBJECT_ID,
-    },
-    query: {
-      type: "BasicCheckCredential",
-      error: "",
-    },
+    (QRCode.toDataURL as jest.Mock).mockResolvedValue("mocked-qrcode");
+    (getCredentialOfferUrl as jest.Mock).mockResolvedValue("mocked-offer-url");
+    (customiseCredentialOfferUrl as jest.Mock).mockReturnValue(
+      "customised-url",
+    );
   });
-  const { res } = getMockRes();
 
   it("should render the credential offer page", async () => {
-    const credentialOfferMocked =
-      "https://mobile.dev.account.gov.uk/wallet-test/add?credential_offer=%7B%22credentials%22%3A%5B%22SocialSecurityCredential%22%5D%2C%22grants%22%3A%7B%22urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Apre-authorized_code%22%3A%7B%22pre-authorized_code%22%3A%22eyJraWQiOiI3OGZhMTMxZDY3N2MxYWMwZjE3MmM1M2I0N2FjMTY5YTk1YWQwZDkyYzM4YmQ3OTRhNzBkYTU5MDMyMDU4Mjc0IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwMDEiLCJjbGllbnRJZCI6IlRFU1RfQ0xJRU5UX0lEIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiY3JlZGVudGlhbF9pZGVudGlmaWVycyI6WyI1ZjM5YTY4Zi02M2MzLTRkMGYtODdlNy0yNGYyNzRjZWJkYWYiXSwiZXhwIjoxNzM2NTA2NzkzLCJpYXQiOjE3MzY1MDY0OTN9.AHeaVwMBqlTOO1Qmgg38-OWiSTs-AmEtLJafz6Ks31CCqHiXJ_QujmK5jJGWpry8X84FSksPhhGoTIG61TbLuQ%22%7D%7D%2C%22credential_issuer%22%3A%22http%3A%2F%2Flocalhost%3A8080%22%7D";
-
-    getCredentialOfferUrl.mockReturnValueOnce(credentialOfferMocked);
-    customiseCredentialOfferUrl.mockReturnValueOnce(
-      `https://mobile.build.account.gov.uk/test-wallet/add?credential_offer=${credentialOfferMocked}`,
-    );
-
     await credentialOfferViewerController(config)(req, res);
 
     expect(getCredentialOfferUrl).toHaveBeenCalledWith(
-      WALLET_SUBJECT_ID,
+      wallletSubjectId,
       "2e0fac05-4b38-480f-9cbd-b046eabe1e46",
       "BasicCheckCredential",
     );
     expect(customiseCredentialOfferUrl).toHaveBeenCalledWith(
-      "https://mobile.dev.account.gov.uk/wallet-test/add?credential_offer=%7B%22credentials%22%3A%5B%22SocialSecurityCredential%22%5D%2C%22grants%22%3A%7B%22urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Apre-authorized_code%22%3A%7B%22pre-authorized_code%22%3A%22eyJraWQiOiI3OGZhMTMxZDY3N2MxYWMwZjE3MmM1M2I0N2FjMTY5YTk1YWQwZDkyYzM4YmQ3OTRhNzBkYTU5MDMyMDU4Mjc0IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwMDEiLCJjbGllbnRJZCI6IlRFU1RfQ0xJRU5UX0lEIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiY3JlZGVudGlhbF9pZGVudGlmaWVycyI6WyI1ZjM5YTY4Zi02M2MzLTRkMGYtODdlNy0yNGYyNzRjZWJkYWYiXSwiZXhwIjoxNzM2NTA2NzkzLCJpYXQiOjE3MzY1MDY0OTN9.AHeaVwMBqlTOO1Qmgg38-OWiSTs-AmEtLJafz6Ks31CCqHiXJ_QujmK5jJGWpry8X84FSksPhhGoTIG61TbLuQ%22%7D%7D%2C%22credential_issuer%22%3A%22http%3A%2F%2Flocalhost%3A8080%22%7D",
-      "test-app-3",
-      config.walletAppsConfig,
-      config.walletApps,
+      "mocked-offer-url",
+      "test-app-1",
+      walletAppsConfig,
+      ["test-app-1", "test-app-2"],
       "",
     );
+    expect(QRCode.toDataURL).toHaveBeenCalledWith("customised-url");
     expect(res.render).toHaveBeenCalledWith("credential-offer.njk", {
-      authenticated: true,
-      qrCode: "data:image/png;base64,iVBORw0KGgoAAAANSU",
-      universalLink:
-        "https://mobile.build.account.gov.uk/test-wallet/add?credential_offer=https://mobile.dev.account.gov.uk/wallet-test/add?credential_offer=%7B%22credentials%22%3A%5B%22SocialSecurityCredential%22%5D%2C%22grants%22%3A%7B%22urn%3Aietf%3Aparams%3Aoauth%3Agrant-type%3Apre-authorized_code%22%3A%7B%22pre-authorized_code%22%3A%22eyJraWQiOiI3OGZhMTMxZDY3N2MxYWMwZjE3MmM1M2I0N2FjMTY5YTk1YWQwZDkyYzM4YmQ3OTRhNzBkYTU5MDMyMDU4Mjc0IiwidHlwIjoiSldUIiwiYWxnIjoiRVMyNTYifQ.eyJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjgwMDEiLCJjbGllbnRJZCI6IlRFU1RfQ0xJRU5UX0lEIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwIiwiY3JlZGVudGlhbF9pZGVudGlmaWVycyI6WyI1ZjM5YTY4Zi02M2MzLTRkMGYtODdlNy0yNGYyNzRjZWJkYWYiXSwiZXhwIjoxNzM2NTA2NzkzLCJpYXQiOjE3MzY1MDY0OTN9.AHeaVwMBqlTOO1Qmgg38-OWiSTs-AmEtLJafz6Ks31CCqHiXJ_QujmK5jJGWpry8X84FSksPhhGoTIG61TbLuQ%22%7D%7D%2C%22credential_issuer%22%3A%22http%3A%2F%2Flocalhost%3A8080%22%7D",
-      environment: config.environment,
+      authenticated: expect.anything(),
+      universalLink: "customised-url",
+      qrCode: "mocked-qrcode",
+      environment: "test",
     });
   });
 
-  it("should render an error page when an error happens", async () => {
-    getCredentialOfferUrl.mockRejectedValueOnce(new Error("SOME_ERROR"));
-
+  it("should render error page when an exception is thrown", async () => {
+    (getCredentialOfferUrl as jest.Mock).mockRejectedValueOnce(
+      new Error("Network error"),
+    );
     await credentialOfferViewerController(config)(req, res);
-
     expect(res.render).toHaveBeenCalledWith("500.njk");
   });
 });
