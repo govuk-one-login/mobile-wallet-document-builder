585193a30e4d72b740728d033f06da127ed15215
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-10-29 11:20:55 +0000

    feat: add configurable app selection per environment (#328)

--- src/credentialOfferViewer/controller.ts
+++ src/credentialOfferViewer/controller.ts
@@ -4,45 +4,60 @@ import { getCredentialOfferUrl } from "./services/credentialOfferService";
 import { customiseCredentialOfferUrl } from "./helpers/customCredentialOfferUrl";
 import { logger } from "../middleware/logger";
 import { isAuthenticated } from "../utils/isAuthenticated";
-import { APPS, getEnvironment } from "../config/appConfig";
+import { getEnvironment, getWalletApps } from "../config/appConfig";
+import { ExpressRouteFunction } from "../types/ExpressRouteFunction";
+import {
+  WalletAppsConfig,
+  walletAppsConfig as config,
+} from "../config/walletAppsConfig";
 
-export async function credentialOfferViewerController(
-  req: Request,
-  res: Response,
-): Promise<void> {
-  try {
-    const { itemId } = req.params;
-    const selectedApp = req.cookies.app as string;
-    const credentialType = req.query.type as string;
-    const errorScenario = req.query.error as string;
-    const walletSubjectId = req.cookies.wallet_subject_id as string;
+export interface CredentialOfferViewerConfig {
+  walletAppsConfig?: WalletAppsConfig;
+  walletApps?: string[];
+  environment?: string;
+}
 
-    const credentialOfferUrl = await getCredentialOfferUrl(
-      walletSubjectId,
-      itemId,
-      credentialType,
-    );
+export function credentialOfferViewerController({
+  walletAppsConfig = config,
+  walletApps = getWalletApps(),
+  environment = getEnvironment(),
+}: CredentialOfferViewerConfig = {}): ExpressRouteFunction {
+  return async function (req: Request, res: Response): Promise<void> {
+    try {
+      const { itemId } = req.params;
+      const selectedApp = req.cookies.app;
+      const credentialType = req.query.type as string;
+      const errorScenario = req.query.error as string;
+      const walletSubjectId = req.cookies.wallet_subject_id;
 
-    const customisedCredentialOfferUrl = customiseCredentialOfferUrl(
-      credentialOfferUrl,
-      selectedApp,
-      APPS,
-      errorScenario,
-    );
+      const credentialOfferUrl = await getCredentialOfferUrl(
+        walletSubjectId,
+        itemId,
+        credentialType,
+      );
 
-    const qrCode = await QRCode.toDataURL(customisedCredentialOfferUrl);
+      const customisedCredentialOfferUrl = customiseCredentialOfferUrl(
+        credentialOfferUrl,
+        selectedApp,
+        walletAppsConfig,
+        walletApps,
+        errorScenario,
+      );
 
-    res.render("credential-offer.njk", {
-      authenticated: isAuthenticated(req),
-      universalLink: customisedCredentialOfferUrl,
-      qrCode,
-      environment: getEnvironment(),
-    });
-  } catch (error) {
-    logger.error(
-      error,
-      "An error happened processing credential offer request",
-    );
-    res.render("500.njk");
-  }
+      const qrCode = await QRCode.toDataURL(customisedCredentialOfferUrl);
+
+      return res.render("credential-offer.njk", {
+        authenticated: isAuthenticated(req),
+        universalLink: customisedCredentialOfferUrl,
+        qrCode,
+        environment,
+      });
+    } catch (error) {
+      logger.error(
+        error,
+        "An error happened processing credential offer request",
+      );
+      return res.render("500.njk");
+    }
+  };
 }
