bf9936995d9b1e229b1b3fd7d3490fb3c9a71561
Darren Dunford <48522654+darrendunford@users.noreply.github.com>
2025-11-30 21:52:16 +0000

    node 22; first working version

--- src/credentialViewer/controller.ts
+++ src/credentialViewer/controller.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from "express";
 import { isAuthenticated } from "../utils/isAuthenticated";
-import { decodeJwt } from "jose";
+import { decodeJwt, errors } from "jose";
 import axios from "axios";
 import { getCriEndpoint, getSelfUrl } from "../config/appConfig";
 import { GrantType } from "../stsStubAccessToken/token/validateTokenRequest";
 import { logger } from "../middleware/logger";
+import {decode, getEncoded, Tag} from "cbor2";
+import { base64UrlDecoder } from "../utils/base64Encoder";
+import {Sign1} from "@auth0/cose";
+
+// utility to automatically/recursively decode bstr values tagged 24 which are themselves encoded CBOR
+Tag.registerDecoder(24, ({contents}) => {
+  return decode(contents as Buffer);
+})
 
 export async function credentialViewerController(
   req: Request,
@@ -22,12 +30,41 @@ export async function credentialViewerController(
     );
 
     const credential = await getCredential(accessToken, proofJwt);
+    logger.info(`Credential ${credential}`);
     let credentialClaims = null;
+    let credentialSignature = null;
+    let credentialSignaturePayload = null;
     try {
-      const payload = decodeJwt(credential);
-      credentialClaims = JSON.stringify(payload);
+      const payload = {
+        decoded: decodeJwt(credential),
+      };
+      logger.info(payload, "Decoded JWT credential");
+      credentialClaims = JSON.stringify(payload.decoded);
     } catch (error) {
-      logger.info(error, "Could not decode JWT credential");
+      logger.error(error, "Could not decode JWT credential - attempting CBOR decode");
+      if (error instanceof errors.JWTInvalid) {
+        logger.info("Attempting CBOR decode")
+        try {
+          credentialClaims = decode(base64UrlDecoder(credential), {
+            saveOriginal: true,
+          })
+          // @ts-expect-error credential is known
+          const rawMso = credentialClaims.issuerAuth
+          // TODO: document[0] should be replaced by a mechanism which searches for docType: "org.iso.18013.5.1.mDL"
+          credentialSignature = Sign1.decode(getEncoded(rawMso)!)
+          credentialSignaturePayload = decode(Buffer.from(credentialSignature.payload))
+          logger.info({
+            credentialClaims,
+            rawMso,
+            credentialSignature,
+            credentialSignaturePayload,
+          }, "CREDENTIAL OUTPUT FROM CBOR DECODING")
+        } catch (error) {
+          logger.error(error, "An error occurred with JWT and/or CBOR decoding")
+        }
+      } else {
+        logger.error(error, "An error occurred with JWT decoding")
+      }
     }
 
     res.render("credential.njk", {
@@ -37,7 +74,43 @@ export async function credentialViewerController(
       accessTokenClaims: JSON.stringify(accessTokenClaims),
       proofJwt,
       credential,
-      credentialClaims: credentialClaims,
+      credentialClaims: {
+        data: credentialClaims,
+        showAllAsClosed: true,
+        sortPropertyNames: false,
+        fileDroppingEnabled: false,
+        // allowEditing: {
+        //   booleanValues: false,
+        //   decimalValues: false,
+        //   numberValues: false,
+        //
+        // }
+      },
+      credentialSignature: {
+        data: credentialSignature,
+        showAllAsClosed: true,
+        sortPropertyNames: false,
+        fileDroppingEnabled: false,
+        // allowEditing: {
+        //   booleanValues: false,
+        //   decimalValues: false,
+        //   numberValues: false,
+        //
+        // }
+      },
+      credentialSignaturePayload: {
+        data: credentialSignaturePayload,
+        showAllAsClosed: true,
+        sortPropertyNames: false,
+        fileDroppingEnabled: false,
+        // allowEditing: {
+        //   booleanValues: false,
+        //   decimalValues: false,
+        //   numberValues: false,
+        //
+        // }
+      },
+
     });
   } catch (error) {
     logger.error(error, "An error happened.");
