d0dcc768e13a37cd3f942aed03be3df94ccb9513
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-12-18 14:01:17 +0000

    Revert "feat: Simple Document 'throw an error' component is not visible in st…"
    This reverts commit 08ce38de73d560e26a885329abd5434175ffd1ee.

--- test/simpleDocumentBuilder/controller.test.ts
+++ test/simpleDocumentBuilder/controller.test.ts
@@ -2,12 +2,16 @@ import {
   simpleDocumentBuilderGetController,
   simpleDocumentBuilderPostController,
 } from "../../src/simpleDocumentBuilder/controller";
+import { readFileSync } from "fs";
 import * as databaseService from "../../src/services/databaseService";
 import * as s3Service from "../../src/services/s3Service";
-import * as photoUtils from "../../src/utils/photoUtils";
 import { getMockReq, getMockRes } from "@jest-mock/express";
+import * as path from "path";
 import { SimpleDocumentRequestBody } from "../../src/simpleDocumentBuilder/types/SimpleDocumentRequestBody";
 import { ERROR_CHOICES } from "../../src/utils/errorChoices";
+process.env.PHOTOS_BUCKET_NAME = "photosBucket";
+process.env.ENVIRONMENT = "local";
+process.env.DOCUMENTS_TABLE_NAME = "testTable";
 
 jest.mock("node:crypto", () => ({
   randomUUID: jest.fn().mockReturnValue("2e0fac05-4b38-480f-9cbd-b046eabe1e46"),
@@ -18,18 +22,10 @@ jest.mock("../../src/services/databaseService", () => ({
 jest.mock("../../src/services/s3Service", () => ({
   uploadPhoto: jest.fn(),
 }));
-jest.mock("../../src/utils/photoUtils", () => ({
-  getPhoto: jest.fn(),
-}));
+jest.mock("fs");
 jest.mock("../../src/utils/getRandomIntInclusive", () => ({
   getRandomIntInclusive: jest.fn().mockReturnValue(550000),
 }));
-
-const config = {
-  environment: "test",
-  tableName: "documentsTable",
-  bucketName: "photosBucket",
-};
 
 describe("controller.ts", () => {
   beforeAll(() => {
@@ -46,7 +42,7 @@ describe("controller.ts", () => {
       const req = getMockReq({ cookies: { id_token: "id_token" } });
       const { res } = getMockRes();
 
-      await simpleDocumentBuilderGetController(config)(req, res);
+      await simpleDocumentBuilderGetController(req, res);
 
       expect(res.render).toHaveBeenCalledWith(
         "simple-document-details-form.njk",
@@ -86,65 +82,9 @@ describe("controller.ts", () => {
           ],
           errorChoices: ERROR_CHOICES,
           authenticated: true,
-          showThrowError: true,
         },
       );
     });
-
-    test.each([
-      [false, "staging"],
-      [true, "not-staging"],
-    ])(
-      "should set showThrowError to %s when environment is %s",
-      async (expectedValue, environment) => {
-        const req = getMockReq({ cookies: {} });
-        const { res } = getMockRes();
-
-        await simpleDocumentBuilderGetController({ environment })(req, res);
-
-        expect(res.render).toHaveBeenCalledWith(
-          "simple-document-details-form.njk",
-          {
-            defaultIssueDate: {
-              day: "02",
-              month: "05",
-              year: "2025",
-            },
-            defaultExpiryDate: {
-              day: "01",
-              month: "05",
-              year: "2035",
-            },
-            documentNumber: "FLN550000",
-            fishTypeOptions: [
-              {
-                selected: true,
-                text: "Coarse fish",
-                value: "Coarse fish",
-              },
-              {
-                selected: false,
-                text: "Salmon and trout",
-                value: "Salmon and trout",
-              },
-              {
-                selected: false,
-                text: "Sea fishing",
-                value: "Sea fishing",
-              },
-              {
-                selected: false,
-                text: "All freshwater fish",
-                value: "All freshwater fish",
-              },
-            ],
-            errorChoices: ERROR_CHOICES,
-            authenticated: false,
-            showThrowError: expectedValue,
-          },
-        );
-      },
-    );
 
     it("should render the 500 error page if an error is thrown", async () => {
       const req = getMockReq({ cookies: { id_token: "id_token" } });
@@ -154,7 +94,7 @@ describe("controller.ts", () => {
         throw new Error("Rendering error");
       });
 
-      await simpleDocumentBuilderGetController(config)(req, res);
+      await simpleDocumentBuilderGetController(req, res);
 
       expect(res.render).toHaveBeenCalledWith("500.njk");
     });
@@ -164,14 +104,143 @@ describe("controller.ts", () => {
     const requestBody = buildSimpleDocumentRequestBody();
 
     const photoBuffer = Buffer.from("mock photo data");
-    const mockGetPhoto = photoUtils.getPhoto as jest.Mock;
-    mockGetPhoto.mockReturnValue({ photoBuffer, mimeType: "image/jpeg" });
+    const mockReadFileSync = readFileSync as jest.Mock;
+    mockReadFileSync.mockReturnValue(photoBuffer);
 
     const saveDocument = databaseService.saveDocument as jest.Mock;
     const uploadPhoto = s3Service.uploadPhoto as jest.Mock;
 
-    describe("should re-render the form with an error", () => {
-      it("when a date has empty fields", async () => {
+    describe("given an error happens trying to process the request", () => {
+      it("should render the error page", async () => {
+        saveDocument.mockRejectedValueOnce(new Error("SOME_DATABASE_ERROR"));
+        const req = getMockReq({
+          body: requestBody,
+        });
+        const { res } = getMockRes();
+
+        await simpleDocumentBuilderPostController(req, res);
+
+        expect(res.render).toHaveBeenCalledWith("500.njk");
+      });
+    });
+
+    describe.each([
+      ["JPEG", "420x525.jpg", "image/jpeg"],
+      ["PNG", "100x125.png", "image/png"],
+      ["JFIF", "photo.jfif", "image/jpeg"],
+    ])(
+      "given a file of type %s is to be uploaded",
+      (fileType, fileName, mimeType) => {
+        it(`should call the function to upload the photo with the correct arguments`, async () => {
+          const req = getMockReq({
+            body: {
+              ...requestBody,
+              ...{ portrait: fileName },
+            },
+          });
+          const { res } = getMockRes();
+
+          await simpleDocumentBuilderPostController(req, res);
+
+          const expectedPath = path.resolve(
+            __dirname,
+            "../../src/resources",
+            fileName,
+          );
+          expect(mockReadFileSync).toHaveBeenCalledWith(expectedPath);
+          expect(uploadPhoto).toHaveBeenCalledWith(
+            photoBuffer,
+            "2e0fac05-4b38-480f-9cbd-b046eabe1e46",
+            "photosBucket",
+            mimeType,
+          );
+        });
+      },
+    );
+
+    describe("given the photo has been stored successfully", () => {
+      it("should call the function to save the document with the correct arguments", async () => {
+        const req = getMockReq({
+          body: requestBody,
+        });
+        const { res } = getMockRes();
+
+        await simpleDocumentBuilderPostController(req, res);
+
+        expect(saveDocument).toHaveBeenCalledWith("testTable", {
+          itemId: "2e0fac05-4b38-480f-9cbd-b046eabe1e46",
+          documentId: "FLN550000",
+          vcType:
+            "uk.gov.account.mobile.example-credential-issuer.simplemdoc.1",
+          timeToLive: 1748736000,
+          credentialTtlMinutes: 43200,
+          data: {
+            family_name: "Smith",
+            given_name: "John",
+            portrait: "s3://photosBucket/2e0fac05-4b38-480f-9cbd-b046eabe1e46",
+            birth_date: "15-06-1985",
+            issue_date: "01-04-2024",
+            expiry_date: "01-04-2029",
+            issuing_country: "GB",
+            document_number: "FLN550000",
+            type_of_fish: "Sea fishing",
+            number_of_fishing_rods: "2",
+          },
+        });
+      });
+    });
+
+    describe("given the document and photo have been stored successfully", () => {
+      describe("when an unknown error code has been received in the request body", () => {
+        it("should redirect to the credential offer page with only the simple document credential type in the query params", async () => {
+          const req = getMockReq({
+            body: requestBody,
+          });
+          const { res } = getMockRes();
+
+          await simpleDocumentBuilderPostController(req, res);
+
+          expect(res.redirect).toHaveBeenCalledWith(
+            "/view-credential-offer/2e0fac05-4b38-480f-9cbd-b046eabe1e46?type=uk.gov.account.mobile.example-credential-issuer.simplemdoc.1",
+          );
+        });
+      });
+
+      describe("when an error scenario has not been selected", () => {
+        it("should redirect to the credential offer page with only the simple document credential type in the query params", async () => {
+          const req = getMockReq({
+            body: requestBody,
+          });
+          const { res } = getMockRes();
+
+          await simpleDocumentBuilderPostController(req, res);
+
+          expect(res.redirect).toHaveBeenCalledWith(
+            "/view-credential-offer/2e0fac05-4b38-480f-9cbd-b046eabe1e46?type=uk.gov.account.mobile.example-credential-issuer.simplemdoc.1",
+          );
+        });
+      });
+
+      describe("when an error scenario has been selected", () => {
+        it.each(["ERROR:GRANT", "ERROR:500", "ERROR:401", "ERROR:CLIENT"])(
+          "should redirect with the correct error parameter when selectedError is '%s'",
+          async (selectedError) => {
+            const req = getMockReq({
+              body: { ...requestBody, ...{ throwError: selectedError } },
+            });
+            const { res } = getMockRes();
+            await simpleDocumentBuilderPostController(req, res);
+
+            expect(res.redirect).toHaveBeenCalledWith(
+              `/view-credential-offer/2e0fac05-4b38-480f-9cbd-b046eabe1e46?type=uk.gov.account.mobile.example-credential-issuer.simplemdoc.1&error=${selectedError}`,
+            );
+          },
+        );
+      });
+    });
+
+    describe("given invalid date fields", () => {
+      it("should render an error when the birthdate has empty fields", async () => {
         const body = buildSimpleDocumentRequestBody({
           "birth-day": "",
           "birth-month": "08",
@@ -182,7 +251,7 @@ describe("controller.ts", () => {
           cookies: { id_token: "id_token" },
         });
         const { res } = getMockRes();
-        await simpleDocumentBuilderPostController(config)(req, res);
+        await simpleDocumentBuilderPostController(req, res);
         expect(res.render).toHaveBeenCalledWith(
           "simple-document-details-form.njk",
           {
@@ -224,13 +293,14 @@ describe("controller.ts", () => {
               },
             ],
             authenticated: true,
-            showThrowError: true,
           },
         );
         expect(res.redirect).not.toHaveBeenCalled();
       });
+    });
 
-      it("when the type of fish selected is unknown", async () => {
+    describe("given an invalid type of fish", () => {
+      it("should render an error when the type of fish selected is unknown", async () => {
         const body = buildSimpleDocumentRequestBody({
           type_of_fish: "Unknwon fish type",
         });
@@ -239,7 +309,7 @@ describe("controller.ts", () => {
           cookies: { id_token: "id_token" },
         });
         const { res } = getMockRes();
-        await simpleDocumentBuilderPostController(config)(req, res);
+        await simpleDocumentBuilderPostController(req, res);
         expect(res.render).toHaveBeenCalledWith(
           "simple-document-details-form.njk",
           {
@@ -281,136 +351,9 @@ describe("controller.ts", () => {
               },
             ],
             authenticated: true,
-            showThrowError: true,
           },
         );
         expect(res.redirect).not.toHaveBeenCalled();
-      });
-    });
-
-    describe("given an error happens trying to process the request", () => {
-      it("should render the error page", async () => {
-        saveDocument.mockRejectedValueOnce(new Error("SOME_DATABASE_ERROR"));
-        const req = getMockReq({
-          body: requestBody,
-        });
-        const { res } = getMockRes();
-
-        await simpleDocumentBuilderPostController(config)(req, res);
-
-        expect(res.render).toHaveBeenCalledWith("500.njk");
-      });
-    });
-
-    describe.each([
-      ["JPEG", "420x525.jpg", "image/jpeg"],
-      ["PNG", "100x125.png", "image/png"],
-      ["JFIF", "photo.jfif", "image/jpeg"],
-    ])(
-      "given a file of type %s is to be uploaded",
-      (fileType, fileName, mimeType) => {
-        it(`should call the function to upload the photo with the correct arguments`, async () => {
-          const req = getMockReq({
-            body: {
-              ...requestBody,
-              ...{ portrait: fileName },
-            },
-          });
-          const { res } = getMockRes();
-
-          mockGetPhoto.mockReturnValue({ photoBuffer, mimeType });
-
-          await simpleDocumentBuilderPostController(config)(req, res);
-
-          expect(mockGetPhoto).toHaveBeenCalledWith(fileName);
-          expect(uploadPhoto).toHaveBeenCalledWith(
-            photoBuffer,
-            "2e0fac05-4b38-480f-9cbd-b046eabe1e46",
-            "photosBucket",
-            mimeType,
-          );
-        });
-      },
-    );
-
-    describe("given the photo has been stored successfully", () => {
-      it("should call the function to save the document with the correct arguments", async () => {
-        const req = getMockReq({
-          body: requestBody,
-        });
-        const { res } = getMockRes();
-
-        await simpleDocumentBuilderPostController(config)(req, res);
-
-        expect(saveDocument).toHaveBeenCalledWith("documentsTable", {
-          itemId: "2e0fac05-4b38-480f-9cbd-b046eabe1e46",
-          documentId: "FLN550000",
-          vcType:
-            "uk.gov.account.mobile.example-credential-issuer.simplemdoc.1",
-          timeToLive: 1748736000,
-          credentialTtlMinutes: 43200,
-          data: {
-            family_name: "Smith",
-            given_name: "John",
-            portrait: "s3://photosBucket/2e0fac05-4b38-480f-9cbd-b046eabe1e46",
-            birth_date: "15-06-1985",
-            issue_date: "01-04-2024",
-            expiry_date: "01-04-2029",
-            issuing_country: "GB",
-            document_number: "FLN550000",
-            type_of_fish: "Sea fishing",
-            number_of_fishing_rods: "2",
-          },
-        });
-      });
-    });
-
-    describe("given the document and photo have been stored successfully", () => {
-      describe("when an unknown error code has been received in the request body", () => {
-        it("should redirect to the credential offer page with only the simple document credential type in the query params", async () => {
-          const req = getMockReq({
-            body: requestBody,
-          });
-          const { res } = getMockRes();
-
-          await simpleDocumentBuilderPostController(config)(req, res);
-
-          expect(res.redirect).toHaveBeenCalledWith(
-            "/view-credential-offer/2e0fac05-4b38-480f-9cbd-b046eabe1e46?type=uk.gov.account.mobile.example-credential-issuer.simplemdoc.1",
-          );
-        });
-      });
-
-      describe("when an error scenario has not been selected", () => {
-        it("should redirect to the credential offer page with only the simple document credential type in the query params", async () => {
-          const req = getMockReq({
-            body: requestBody,
-          });
-          const { res } = getMockRes();
-
-          await simpleDocumentBuilderPostController(config)(req, res);
-
-          expect(res.redirect).toHaveBeenCalledWith(
-            "/view-credential-offer/2e0fac05-4b38-480f-9cbd-b046eabe1e46?type=uk.gov.account.mobile.example-credential-issuer.simplemdoc.1",
-          );
-        });
-      });
-
-      describe("when an error scenario has been selected", () => {
-        it.each(["ERROR:GRANT", "ERROR:500", "ERROR:401", "ERROR:CLIENT"])(
-          "should redirect with the correct error parameter when selectedError is '%s'",
-          async (selectedError) => {
-            const req = getMockReq({
-              body: { ...requestBody, ...{ throwError: selectedError } },
-            });
-            const { res } = getMockRes();
-            await simpleDocumentBuilderPostController(config)(req, res);
-
-            expect(res.redirect).toHaveBeenCalledWith(
-              `/view-credential-offer/2e0fac05-4b38-480f-9cbd-b046eabe1e46?type=uk.gov.account.mobile.example-credential-issuer.simplemdoc.1&error=${selectedError}`,
-            );
-          },
-        );
       });
     });
   });
