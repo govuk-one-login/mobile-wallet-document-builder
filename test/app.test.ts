b3492e7c59d3088d6258cde191da78ac3fe92c41
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2025-12-18 13:10:21 +0000

    test: update failing app tests

--- test/app.test.ts
+++ test/app.test.ts
@@ -1,11 +1,13 @@
-process.env.ENVIRONMENT = "local";
-process.env.OIDC_CLIENT_ID = "client_id";
-process.env.OIDC_PRIVATE_KEY = "private_key";
-process.env.OIDC_ISSUER_DISCOVERY_ENDPOINT = "discovery_endpoint";
-process.env.SELF = "redirect_uri";
+process.env.ENVIRONMENT = "test";
+process.env.OIDC_CLIENT_ID = "client-id";
+process.env.OIDC_PRIVATE_KEY = "private-key";
+process.env.OIDC_ISSUER_DISCOVERY_ENDPOINT = "discovery-endpoint";
+process.env.SELF = "redirect-uri";
 process.env.COOKIE_TTL_IN_MILLISECONDS = "100";
 process.env.CREDENTIAL_ISSUER_URL = "https://test-cri.example.com";
 process.env.WALLET_APPS = "test-app-1,test-app-2";
+process.env.DOCUMENTS_TABLE_NAME = "documents-table-name";
+process.env.PHOTOS_BUCKET_NAME = "photos-bucket-name";
 
 import { createApp } from "../src/app";
 import { expect } from "@jest/globals";
