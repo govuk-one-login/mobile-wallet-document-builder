eacccc9d2fabc9ae70001072e1f11a922eff7d98
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-01-14 11:59:19 +0000

    feat: make it possible to bypass auth

--- src/utils/isAuthenticated.ts
+++ src/utils/isAuthenticated.ts
@@ -1,9 +1,8 @@
 import e from "express";
+import { isAuthBypassed } from "../config/appConfig";
 
 export function isAuthenticated(req: e.Request): boolean {
-  if (req.cookies?.id_token) {
-    return true;
-  } else {
+  if (isAuthBypassed()) {
     return false;
-  }
+  } else return !!req.cookies?.id_token;
 }
