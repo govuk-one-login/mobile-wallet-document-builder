18463bdb1bfeb58de1c68a83de26764b4096216e
Niki Stefanopoulou <niki.stefanopoulou@digital.cabinet-office.gov.uk>
2026-01-28 17:09:38 +0000

    feat: added a controller and router for start-now page

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,7 @@ import { revokeRouter } from "./revoke/router";
 import { refreshRouter } from "./refresh/router";
 import { simpleDocumentBuilderRouter } from "./simpleDocumentBuilder/router";
 import { drivingLicenceBuilderRouter } from "./drivingLicenceBuilder/router";
+import { dvsStartRouter } from "./dvsStart/router";
 import { dvsJourneySelectorRouter } from "./dvsJourneySelector/router";
 
 const APP_VIEWS = [
@@ -94,6 +95,7 @@ export async function createApp(): Promise<express.Application> {
   app.use(ninoDocumentBuilderRouter);
   app.use(returnFromAuthRouter);
   app.use(veteranCardDocumentBuilderRouter);
+  app.use(dvsStartRouter);
 
   return app;
 }
