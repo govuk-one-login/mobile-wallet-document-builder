6a3a23c6c1efd987dc275c637903d1d611ee8dec
Ana Hickmann <ana.hickmann1@digital.cabinet-office.gov.uk>
2026-01-30 09:59:15 +0000

    feat: add DVS journey selector page (#393)
    * feat: add DVS journey selector page
    
    * feat: remove govuk-adding-top-2 from base template
    
    * feat: remove service name from individual templates
    
    * fix: remove extra slash from route

---
+++ src/dvsJourneySelector/router.ts
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  dvsJourneySelectorGetController,
+  dvsJourneySelectorPostController,
+} from "./controller";
+
+const router = express.Router();
+
+router.get("/dvs/select-journey", dvsJourneySelectorGetController);
+router.post("/dvs/select-journey", dvsJourneySelectorPostController);
+
+export { router as dvsJourneySelectorRouter };
